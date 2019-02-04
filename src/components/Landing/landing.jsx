04a16a789abdea6098af6e0497dfe0a674ae96c1
pragyakar <pragyakar75@gmail.com>
2019-02-04 12:02:26 +0545

    :sparkles: Added component layout and initial routes

---
+++ src/components/Landing/landing.jsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+class Landing extends React.Component{
+  render() {
+    return(
+      <div className="container">
+        <p>Dummy Landing Page</p>
+      </div>
+    )
+  }
+ }
+
+export default Landing;
