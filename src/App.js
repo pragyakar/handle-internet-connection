cf0f7fbbf4a4a0f421fa5690b05d7fd208883563
pragyakar <pragyakar75@gmail.com>
2019-02-04 14:58:31 +0545

    :hammer: Moved component logic to root component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Landing from './components/Landing';
+import Alert from './components/Alert';
 
 import './App.css';
 
 class App extends Component {
+  state = {
+    errorMessage: '',
+  }
+
+  componentDidMount = () => {
+    window.addEventListener('offline', () => {
+      this.setState({
+        errorMessage: 'No Internet Connection Detected... ',
+      })
+    })
+  }
+
+  componentDidUpdate = () => {
+    window.addEventListener('online', () => {
+      this.setState({
+        errorMessage: '',
+      })
+    })
+  }
 
   render() {
     return (
@@ -12,6 +32,7 @@ class App extends Component {
         <Router>
           <Route exact path="/" component={Landing} />
         </Router>
+        { this.state.errorMessage && <Alert message={this.state.errorMessage} />}
       </div>
     );
   }
--- src/components/Landing/landing.jsx
+++ src/components/Landing/landing.jsx
@@ -1,32 +1,11 @@
 import * as React from 'react';
-import Alert from '../Alert';
 
 class Landing extends React.Component{
-  state = {
-    errorMessage: '',
-  }
-
-  componentDidMount = () => {
-    window.addEventListener('offline', () => {
-      this.setState({
-        errorMessage: 'No Internet Connection Detected... ',
-      })
-    })
-  }
-
-  componentDidUpdate = () => {
-    window.addEventListener('online', () => {
-      this.setState({
-        errorMessage: '',
-      })
-    })
-  }
 
   render() {
     return(
       <div className="landing">
-        <p className="title">Handle Internet Connection</p>
-        { this.state.errorMessage && <Alert message={this.state.errorMessage} />}
+        <p className="title">Handle Internet Connection</p>        
       </div>
     )
   }
